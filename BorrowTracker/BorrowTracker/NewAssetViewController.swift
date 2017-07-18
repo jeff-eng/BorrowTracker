d3d780ec6d458136f592d8eaa4c81fec111775a6
Jeff Eng <jeff@jeffeng.me>
2017-07-17 20:53:10 -0700

    Created interface for adding new Asset in thet storyboard. Also created a NewAssetViewController to customize the behavior of the new asset creation screen.

---
+++ BorrowTracker/BorrowTracker/NewAssetViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NewAssetViewController.swift
+//  BorrowTracker
+//
+//  Created by Jeffrey Eng on 7/17/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class NewAssetViewController: UIViewController {
+ 
+    @IBAction func cancelButton(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Abort New Item", message: "Are you sure you want to exit item creation?", preferredStyle: .actionSheet)
+        let confirmAction = UIAlertAction(title: "Yes", style: .cancel) { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        let cancelAction = UIAlertAction(title: "No", style: .default, handler: nil)
+        alertController.addAction(cancelAction)
+        alertController.addAction(confirmAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+
+}
